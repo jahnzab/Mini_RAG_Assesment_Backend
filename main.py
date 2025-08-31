3d88519d5debdee25a536b27b9c4f21dbc5ce148
Jahanzaib Farooq <157870830+jahnzab@users.noreply.github.com>
2025-08-31 13:39:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -169,7 +169,7 @@ app = FastAPI(
 # Enable CORS for frontend connection
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000",""],
+    allow_origins=["http://localhost:3000","https://mini-rag-assesment-front.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
